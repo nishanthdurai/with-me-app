52d38b24d88537baa18b3408ffefa843dac8f41a
Nishanth Duraisamy <nishanthduraisamy@gmail.com>
2024-09-22 20:11:17 +0530

    main activity created action bar added

--- app/src/main/java/com/tutorconnect/withmeapp/MainActivity.java
+++ app/src/main/java/com/tutorconnect/withmeapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.custom_toolbar);
+        setSupportActionBar(toolbar);
 
     }
 }
